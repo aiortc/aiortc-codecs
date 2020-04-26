2ad89fe5fde6e7faf9c48e8453378170385e25ab
Jeremy Lainé <jeremy.laine@m4x.org>
2020-04-26 15:25:12 +0200

    Try to build codecs on Windows too

---
+++ scripts/build-codecs.bat
@@ -0,0 +1,24 @@
+set destdir=%1
+
+set outputfile=output\codecs-win%PYTHON_ARCH%.tar.bz2
+if "%PYTHON_ARCH%" == "64" (
+    set triplet=x64-windows-static
+) else (
+    set triplet=x86-windows-static
+)
+
+vcpkg install libvpx:%triplet% opus:%triplet%
+
+if exist %destdir% (
+    rmdir /s /q %destdir%
+)
+mkdir %destdir%
+mkdir %destdir%\include
+mkdir %destdir%\lib
+xcopy C:\vcpkg\installed\%triplet%\include %destdir%\include\ /E
+xcopy C:\vcpkg\installed\%triplet%\lib %destdir%\lib\ /E
+
+if not exist output (
+    mkdir output
+)
+tar cjvf %outputfile% -C %destdir% include lib
--- setup.py
+++ setup.py
@@ -4,9 +4,11 @@ import sys
 if sys.platform == "win32":
     include_dirs = ["C:\\cibw\\vendor\\include"]
     library_dirs = ["C:\\cibw\\vendor\\lib"]
+    libraries = ["opus", "vpxmt"]
 else:
     include_dirs = ["/tmp/vendor/include"]
     library_dirs = ["/tmp/vendor/lib"]
+    libraries = ["opus", "vpx"]
 
 setuptools.setup(
     name="dummy",
@@ -17,7 +19,7 @@ setuptools.setup(
             "dummy.binding",
             include_dirs=include_dirs,
             library_dirs=library_dirs,
-            libraries=["opus", "vpx",],
+            libraries=libraries,
             sources=["src/dummy/binding.c"],
         ),
     ],
