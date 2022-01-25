a3ecb3c6feb01dc8f7e029512f9e86bbf03d73a5
Jeremy Lainé <jeremy.laine@m4x.org>
2022-01-25 10:12:22 +0100

    Build codecs for macos on arm64

--- scripts/build-codecs.bat
+++ scripts/build-codecs.bat
@@ -2,10 +2,13 @@ set destdir=%1
 
 set outputfile=output\codecs-win%PYTHON_ARCH%.tar.gz
 if "%PYTHON_ARCH%" == "64" (
+    set platform=win_amd64
     set triplet=x64-windows-static
 ) else (
+    set platform=win32
     set triplet=x86-windows-static
 )
+set outputfile=output\codecs-%platform%.tar.gz
 
 vcpkg install libvpx:%triplet% opus:%triplet%
 
